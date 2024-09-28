52dc811220e2625333da4d5af0f2cf4291b467d2
sandrogegechkori <sandrogegechkori6@gmail.com>
2024-09-28 16:22:09 +0400

    sandro

---
+++ day0/lesson0.py
@@ -0,0 +1,35 @@
+name = "sandro"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭელები სიმბოლო 
+# "sandro" არის ცვლადისთვის მნიშვნელობა
+
+surname = "gegechkori"
+
+# print(name)
+#პრინტ ფუქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "sandro" #ეს არის str (string) ტიპის ცვლადი
+age = 15 #ეს არის int (integer) მთელი რიცხვი 
+height =165 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი 
+
+knows_programming = False #True ან False
+is_ugly =False
+
+isUgly = False #ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname )
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+
+print(name + " " + str(age))
+
+print(name + " " + surname +" " +str(age) +" " + str(height))
