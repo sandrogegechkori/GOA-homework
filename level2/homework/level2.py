a84335d172023e4340cfb2da6f25427f08b5a6f4
sandrogegechkori <sandrogegechkori6@gmail.com>
2024-10-10 00:02:29 +0400

    sadnro

---
+++ level2/homework/level2.py
@@ -0,0 +1,10 @@
+ua = "sandro gegechkori"
+print(ua)
+kik = "goa is best academy in the world"
+print(kik)
+mar ="i love fotball"
+print(mar)
+op = "goa is my future"
+print(op)
+vn = "hard work"
+print(vn)
