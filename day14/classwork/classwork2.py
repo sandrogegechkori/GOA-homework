8603016d573cd68cbd8be707be0092256e7fd817
sandrogegechkori <sandrogegechkori6@gmail.com>
2024-11-20 23:27:59 +0400

    sandro

---
+++ day14/classwork/classwork2.py
@@ -0,0 +1,4 @@
+num1=int(input("enter first number"))
+num2=int(input("enter first number"))
+for i in range(num1, num2):
+    print(i**2)
---
+++ day14/classwork/classwork3.py
@@ -0,0 +1,3 @@
+n = int(input("enter your number :"))
+for i in range(n):
+    print(i*i)
